05cc8f73be8d8abb1f542daeaa4c35c623f059f6
Lukas Graessle <lukas.graessle@web.de>
2016-01-29 12:22:23 +0100

    feat(upload): add static file upload

--- app.js
+++ app.js
@@ -5,27 +5,40 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+
 var routes = require('./routes/index');
 var getUsers = require('./routes/getUsersRoute');
 var setUsers = require('./routes/setUsersRoute');
 
+
+var multer  = require('multer');
+var upload = multer({ dest: 'uploads/' });
+
 var app = express();
+
+app.post('/profile', upload.single('avatar'), function (req, res, next) {
+    console.log(req.file);
+    // req.file is the `avatar` file
+    // req.body will hold the text fields, if there were any
+});
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'img/favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'img/favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
 require('mongodb');
 var monk = require('monk');
-var db = monk('localhost:27017/test');
+var db = monk('192.168.99.100:27017/test');
 // Make our db accessible to our router
 app.use(function (req, res, next) {
     req.db = db;
@@ -36,35 +49,12 @@ app.use('/', routes);
 app.use('/', getUsers);
 app.use('/', setUsers);
 
+
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
     next(err);
-});
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function (err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function (err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
 });
 
 module.exports = app;
