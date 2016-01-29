d3bf05e3f11bfb905af7c9460a23c8e65679ab5c
Michael Hoppe <Michael.Hoppe@gbtec.de>
2016-01-29 11:22:06 +0100

    chore(styling): add tinycolor, do some css

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -5,49 +5,52 @@
     ]);
 
     angular.module('staffbook')
-        .directive('crewList', ['$mdDialog', '$mdMedia', crewList])
+        .directive('crewList', ['$mdDialog', '$mdToast', '$document', crewList])
         .directive("colorMe", function () {
             return {
                 restrict: 'EA',
                 replace: false,
                 link: function (scope, element) {
-                    var color = '#' + (Math.random() * 0xFFFFFF << 0).toString(16);
+                    var colors =  ["#f5f5f5", "#b9f6ca", "#ffff8d", "#84ffff", "#80d8ff", "#448aff", "#b388ff", "#8c9eff", "#ff8a80", "#ff8a80"];
+                    var color = colors[Math.floor(Math.random() * colors.length) + 1];
+
                     element.css('background-color', color);
+                    element.find('md-icon').css('color', tinycolor(color).tetrad()[2]);
 
                 }
             }
         });
 
-    function crewList($mdDialog) {
+    function crewList($mdDialog,$mdToast, $document) {
         return {
             restrict: 'E',
             templateUrl: '/scripts/views/crew.html',
             link: function (scope) {
                 var colors =  ["pink", "blue", "purple", "deepBlue", "lightPurple", "yellow", "green", "red"];
                 console.log(colors[Math.floor(Math.random() *colors.length) + 1]);
-                scope.tiles = [
+                scope.users = [
                     {name: "Hans Wurst", date: new Date(2016, 0, 27), background: colors[Math.floor(Math.random() *colors.length) + 1]},
-                    {name: "Hans Wurst", date: new Date(),  background: colors[Math.floor(Math.random() *colors.length) + 1]},
-                    {name: "Hans Wurst", date: new Date(),  background: colors[Math.floor(Math.random() *colors.length) + 1]},
                     {name: "Hans Wurst", date: new Date(1987,0,29),  background: colors[Math.floor(Math.random() *colors.length) + 1]},
-                    {name: "Hans Wurst", date: new Date(),  background: colors[Math.floor(Math.random() *colors.length) + 1]},
-                    {name: "Hans Wurst", date: new Date(),  background: colors[Math.floor(Math.random() *colors.length) + 1]},
-                    {name: "Hans Wurst", date: new Date(),  background: colors[Math.floor(Math.random() *colors.length) + 1]},
-                    {name: "Hans Wurst", date: new Date(),  background: colors[Math.floor(Math.random() *colors.length) + 1]},
-                    {name: "Hans Wurst", date: new Date(),  background: colors[Math.floor(Math.random() *colors.length) + 1]},
-                    {name: "Hans Wurst", date: new Date(),  background: colors[Math.floor(Math.random() *colors.length) + 1]},
-                    {name: "Hans Wurst", date: new Date(),  background: colors[Math.floor(Math.random() *colors.length) + 1]},
-                    {name: "Hans Wurst", date: new Date(),  background: colors[Math.floor(Math.random() *colors.length) + 1]},
-                    {name: "Hans Wurst", date: new Date(),  background: colors[Math.floor(Math.random() *colors.length) + 1]},
-                    {name: "Hans Wurst", date: new Date(),  background: colors[Math.floor(Math.random() *colors.length) + 1]},
-                    {name: "Hans Wurst", date: new Date(),  background: colors[Math.floor(Math.random() *colors.length) + 1]},
-                    {name: "Hans Wurst", date: new Date(),  background: colors[Math.floor(Math.random() *colors.length) + 1]},
-                    {name: "Hans Wurst", date: new Date(),  background: colors[Math.floor(Math.random() *colors.length) + 1]},
-                    {name: "Hans Wurst", date: new Date(),  background: colors[Math.floor(Math.random() *colors.length) + 1]},
-                    {name: "Hans Wurst", date: new Date(),  background: colors[Math.floor(Math.random() *colors.length) + 1]},
-                    {name: "Hans Wurst", date: new Date(),  background: colors[Math.floor(Math.random() *colors.length) + 1]},
-                    {name: "Hans Wurst", date: new Date(),  background: colors[Math.floor(Math.random() *colors.length) + 1]},
-                    {name: "Hans Wurst", date: new Date(),  background: colors[Math.floor(Math.random() *colors.length) + 1]}
+                    {name: "Hans Wurst", date: new Date(1987,0,28),  background: colors[Math.floor(Math.random() *colors.length) + 1]},
+                    {name: "Hans Wurst", date: new Date(1987,0,28),  background: colors[Math.floor(Math.random() *colors.length) + 1]},
+                    {name: "Hans Wurst", date: new Date(1987,0,28),  background: colors[Math.floor(Math.random() *colors.length) + 1]},
+                    {name: "Hans Wurst", date: new Date(1987,0,28),  background: colors[Math.floor(Math.random() *colors.length) + 1]},
+                    {name: "Hans Wurst", date: new Date(1987,0,28),  background: colors[Math.floor(Math.random() *colors.length) + 1]},
+                    {name: "Hans Wurst", date: new Date(1987,0,28),  background: colors[Math.floor(Math.random() *colors.length) + 1]},
+                    {name: "Hans Wurst", date: new Date(1987,0,28),  background: colors[Math.floor(Math.random() *colors.length) + 1]},
+                    {name: "Hans Wurst", date: new Date(1987,0,28),  background: colors[Math.floor(Math.random() *colors.length) + 1]},
+                    {name: "Hans Wurst", date: new Date(1987,0,28),  background: colors[Math.floor(Math.random() *colors.length) + 1]},
+                    {name: "Hans Wurst", date: new Date(1987,0,28),  background: colors[Math.floor(Math.random() *colors.length) + 1]},
+                    {name: "Hans Wurst", date: new Date(1987,0,28),  background: colors[Math.floor(Math.random() *colors.length) + 1]},
+                    {name: "Hans Wurst", date: new Date(1987,0,29),  background: colors[Math.floor(Math.random() *colors.length) + 1]},
+                    {name: "Hans Wurst", date: new Date(1987,0,28),  background: colors[Math.floor(Math.random() *colors.length) + 1]},
+                    {name: "Hans Wurst", date: new Date(1987,0,28),  background: colors[Math.floor(Math.random() *colors.length) + 1]},
+                    {name: "Hans Wurst", date: new Date(1987,0,28),  background: colors[Math.floor(Math.random() *colors.length) + 1]},
+                    {name: "Hans Wurst", date: new Date(1987,0,28),  background: colors[Math.floor(Math.random() *colors.length) + 1]},
+                    {name: "Hans Wurst", date: new Date(1987,0,28),  background: colors[Math.floor(Math.random() *colors.length) + 1]},
+                    {name: "Hans Wurst", date: new Date(1987,0,28),  background: colors[Math.floor(Math.random() *colors.length) + 1]},
+                    {name: "Hans Wurst", date: new Date(1987,0,28),  background: colors[Math.floor(Math.random() *colors.length) + 1]},
+                    {name: "Hans Wurst", date: new Date(1987,0,28),  background: colors[Math.floor(Math.random() *colors.length) + 1]}
                 ];
 
                 scope.showDetail = function (member,ev) {
@@ -63,19 +66,40 @@
                         parent: angular.element(document.body),
                         targetEvent: ev,
                         clickOutsideToClose: true
-                    })
-                        .then(function (answer) {
-                            scope.status = 'You said the information was "' + answer + '".';
-                        }, function () {
-                            scope.status = 'You cancelled the dialog.';
-                        });
-
+                    });
                 };
+
+
+
+                scope.usersHaveBirthday = function() {
+                    var bList = scope.users.filter(function(user) {
+                        return scope.hasBirthday(user.date);
+                    });
+                    scope.showBirthdayToast(bList);
+                }
 
                 scope.hasBirthday = function(bDate) {
                     var date =  new Date();
                     return bDate.getDate() === date.getDate() && bDate.getMonth() === date.getMonth();
+
                 }
+
+                scope.showBirthdayToast = function(bList) {
+                    function ToastController($scope,$mdToast) {
+                        $scope.bList = bList;
+                        $scope.closeToast = function() {
+                            $mdToast.hide();
+                        };
+                    }
+                    $mdToast.show({
+                        controller: ToastController,
+                        templateUrl: 'scripts/views/toast.tmpl.html',
+                        parent : $document[0].querySelector('#toaster'),
+                        hideDelay: 6000,
+                        position: 'top right'
+                    });
+                };
+                scope.usersHaveBirthday();
             }
         };
     }
